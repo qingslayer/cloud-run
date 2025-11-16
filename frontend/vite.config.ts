512f8b8cf914ef6d26affaa49a8562bfab0aaa88
qingy <199672678+letsgitlost@users.noreply.github.com>
2025-11-17 01:05:34 +0700

    smol tweaks for prod

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+    // Load env file based on `mode` in the current working directory.
+    // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+    const env = loadEnv(mode, process.cwd(), '');
     return {
       server: {
         port: 5173,
